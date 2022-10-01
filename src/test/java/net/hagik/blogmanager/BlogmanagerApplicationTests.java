bb2ec09fb908d8509045a77db8fb868ba8d47d48
Hariton Andrei Marius <andreimariushariton@gmail.com>
2022-10-01 23:41:01 +0200

    First commit

---
+++ src/test/java/net/hagik/blogmanager/BlogmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hagik.blogmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
